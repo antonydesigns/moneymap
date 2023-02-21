36cf9cc9650cf00c89315cb7fbbbffa3733e358e
gio <gio>
2023-02-21 17:51:42 +0800

    borrowing sketch css to expand on tailwind

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "./assets/css/tailwind-output.css";
 import "./assets/css/global.css";
 
 function App() {
-  return (
-    <div className="mx-16 border border-black">
-      <p className="mx-16  bg-green-400  text-red-500 ">test branch</p>
-    </div>
-  );
+  return <div className="do-not-delete"></div>;
 }
 
 export default App;
