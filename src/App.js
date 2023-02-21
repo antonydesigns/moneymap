674dfea13c5a6d8be05430a8660ca66ff5f16587
gio <gio>
2023-02-21 17:17:26 +0800

    test run with hello mom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./assets/css/tailwind-output.css";
+import "./assets/css/global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-16 border border-black">
+      <p className="mx-16  bg-green-400  text-red-500 ">helloo Mom</p>
     </div>
   );
 }
