befe414e8a86c5ec9bfd4756de987555e7c14bc1
gio <gio>
2023-02-27 15:40:42 +0800

    nav cosmetics

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <nav className="mt-4">
+    <nav className="bg-green-300 py-4">
       <div className="logo mid text-3xl font-bold">Money Map</div>
       <div className="navbar mid">
         <Link
-          className="navlink mx-2 mt-5 rounded border border-black px-2 hover:bg-green-300 active:bg-yellow-200"
+          className="navlink mx-2 mt-5 rounded border border-black bg-green-400 px-2 hover:bg-green-200"
           to="/"
         >
           Home
         </Link>
         <Link
-          className="navlink mx-2 mt-5 rounded border border-black px-2 hover:bg-green-300"
+          className="navlink mx-2 mt-5 rounded border border-black bg-green-400 px-2 hover:bg-green-200"
           to="/create"
         >
           Create New
