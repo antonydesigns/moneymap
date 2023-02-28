7c4cd2d859bd62826c2c74f29d2105be104eaadd
gio <gio>
2023-02-28 15:03:06 +0800

    mobile view

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -12,35 +12,31 @@ function Create() {
   return (
     <div className="page create">
       <form onSubmit={handleSubmit} className="mt-5">
-        <fieldset className="relative right-4">
-          <div className="columns-2">
-            <label htmlFor="username" className="text-right">
-              <p className="p-1 pr-3">Name:</p>
-            </label>
-            <input
-              className="block border border-black p-1"
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="mt-5 columns-2">
-            <label htmlFor="comment" className="text-right">
-              <p className="p-1 pr-3">Comments:</p>
-            </label>
-            <input
-              className="block border border-black p-1"
-              type="text"
-              id="comment"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-            />
-          </div>
+        <fieldset className="">
+          <label htmlFor="username" className="">
+            <p>Name:</p>
+          </label>
+          <input
+            className="block w-full rounded-md border border-black p-1"
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <label htmlFor="comment" className="">
+            <p className="p-1 pr-3">Comments:</p>
+          </label>
+          <textarea
+            className="h-min-20 block w-full rounded-md border border-black p-1"
+            id="comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
         </fieldset>
 
         <div className="mid mt-10">
-          <button className="mid rounded-lg border border-black p-2">
+          <button className="mid rounded-lg border border-black bg-green-400 p-2 hover:bg-green-200">
             Post comment
           </button>
         </div>
